b70aa89b87e4e6fce2e4917db8576744de01b816
Mahmudur <mahmudhera93@gmail.com>
2025-06-01 18:44:23 -0400

    add cifar10 results (xfer learning)

--- cnn/train_and_test_cifar10.py
+++ cnn/train_and_test_cifar10.py
@@ -8,6 +8,8 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from sklearn.model_selection import train_test_split
+import torchvision
+from torchvision import transforms
 import pickle
 
 from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
@@ -18,47 +20,41 @@ torch.set_float32_matmul_precision('high')
 
 # Dataset Class
 class Cifar10Dataset(Dataset):
-    def __init__(self, images, labels):
+    def __init__(self, images, labels, transform=None):
         self.images = torch.tensor(images, dtype=torch.float32).permute(0, 3, 1, 2)
         self.labels = torch.tensor(labels, dtype=torch.long)
+        self.transform = transform
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        return self.images[idx], self.labels[idx]
+        if self.transform:
+            image = self.transform(self.images[idx])
+        return image, self.labels[idx]
     
     
 # CNN LightningModule
 class LitCNN(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # 32x32 → 16x16
-
-            nn.Conv2d(64, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
-            nn.Conv2d(64, 128, 3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # 16x16 → 8x8
-
-            nn.AdaptiveAvgPool2d((1, 1))  # output: (batch, 64, 1, 1)
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, 10)
-        )
+        self.save_hyperparameters()
+        self.model = torchvision.models.resnet50(pretrained=True)
+        
+        for param in self.model.parameters():
+            param.requires_grad = True
+            
+        # Replace the last fully connected layer
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_features, 10)
+        self.net = self.model
+        self.criterion = nn.CrossEntropyLoss()
         
         # tracking training loss
         self.train_loss_this_epoch = 0.0
 
     def forward(self, x):
-        # TODO
         x = self.net(x)
-        x = self.classifier(x)
         return x
 
     def training_step(self, batch, batch_idx):
@@ -72,11 +68,11 @@ class LitCNN(pl.LightningModule):
         x, y = batch
         logits = self(x)
         acc = (logits.argmax(dim=1) == y).float().mean()
-        self.log("val_acc", acc)
+        self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         # compute validation loss
         val_loss = F.cross_entropy(logits, y)
-        self.log("val_loss", val_loss)
-        self.log("train_loss", self.train_loss_this_epoch)
+        self.log("val_loss", val_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        self.log("train_loss", self.train_loss_this_epoch, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         self.train_loss_this_epoch = 0.0
         
 
@@ -88,8 +84,9 @@ class LitCNN(pl.LightningModule):
         
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+        #optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.5)
         return {
             'optimizer': optimizer,
             'lr_scheduler': {
@@ -102,12 +99,21 @@ class LitCNN(pl.LightningModule):
     
 # DataModule for loading and preparing data
 class Cifar10DataModule(pl.LightningDataModule):
-    def __init__(self, train_file_list, test_file, batch_size=256, num_workers=4):
+    def __init__(self, train_file_list, test_file, batch_size=32, num_workers=4):
         super().__init__()
         self.train_file_list = train_file_list
         self.test_file = test_file
         self.batch_size = batch_size
         self.num_workers = num_workers
+        
+        # Define image transforms
+        self.train_val_test_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                [0.229, 0.224, 0.225])
+        ])
 
     def prepare_data(self):
         if not os.path.exists(self.test_file):
@@ -142,9 +148,9 @@ class Cifar10DataModule(pl.LightningDataModule):
         # Split training/validation
         X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1)
 
-        self.train_dataset = Cifar10Dataset(X_train, y_train)
-        self.val_dataset = Cifar10Dataset(X_val, y_val)
-        self.test_dataset = Cifar10Dataset(X_test, y_test)
+        self.train_dataset = Cifar10Dataset(X_train, y_train, transform=self.train_val_test_transform)
+        self.val_dataset = Cifar10Dataset(X_val, y_val, transform=self.train_val_test_transform)
+        self.test_dataset = Cifar10Dataset(X_test, y_test, transform=self.train_val_test_transform)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
@@ -169,15 +175,18 @@ if __name__ == "__main__":
     model = LitCNN()
 
     trainer = Trainer(
-        max_epochs=50,
+        max_epochs=5,
         accelerator='auto',
         #devices='auto',
         log_every_n_steps=1,
-        devices=1,
+        devices=4,
         logger=logger,
     )
 
     trainer.fit(model, datamodule=datamodule)
+    
+    # set trainer's num devices to 1 for testing
+    trainer.strategy.num_devices = 1
     trainer.test(model, datamodule=datamodule)
 
     # Save model
